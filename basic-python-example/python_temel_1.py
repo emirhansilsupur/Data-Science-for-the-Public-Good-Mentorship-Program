ff74acfb01055fa4805c5534fc58d550ab34e242
Emirhan Silsüpür <77552432+sremirhn@users.noreply.github.com>
2021-12-25 14:19:33 +0300

    Update python_temel_1.py

--- basic-python-example/python_temel_1.py
+++ basic-python-example/python_temel_1.py
@@ -3,11 +3,11 @@
 def flatten(l):
     s = []
     for i in l:
-        if type(i) is list:
+        if isinstance(i,list):
             s.extend(flatten(i))
         else:
             s.append(i)
     return s
 l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-print(fluatten(l))
+print(flatten(l))
 -
